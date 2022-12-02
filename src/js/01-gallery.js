971f69c2cbdd68a9a064b9759b6e768b2e24dfce
Maksim1903 <dovgusha.m@gmail.com>
2022-12-02 23:30:27 +0100

    z

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,58 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
+// Change code below this line
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const gallery = createGallery(galleryItems);
+
+galleryContainer.innerHTML = gallery;
+galleryContainer.addEventListener('click', clickOnGallery);
+
+function clickOnGallery(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  console.log(event.target.nodeName);
+  let instance = new SimpleLightbox('.gallery', {});
+  <div class="modal">
+    <a href="images/image1.jpg">
+      <img src="${event.target.dataset.source}" alt="" title="" />
+    </a>
+  </div>;
+
+  //   <div class="modal">
+  //      <img src="${event.target.dataset.source}" alt="Big Pictures" width="800" height="600">
+  //   </div>
+
+  //   onShow: instance => {
+  //     galleryContainer.addEventListener('keyup', closeButton);
+  //   },
+  //   onClose: instance => {
+  //     galleryContainer.removeEventListener('keyup', closeButton);
+  //   },
+
+  function closeButton(event) {
+    if (event.key === 'Escape') {
+      instance.close();
+    }
+  }
+
+  instance.show();
+}
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+		    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+	      </a>
+        </div>`;
+    })
+    .join('');
+}
