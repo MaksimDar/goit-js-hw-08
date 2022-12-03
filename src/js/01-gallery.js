1a0912ebc3beb9dfb280fa9aa2104a22ef54a975
Maksim1903 <dovgusha.m@gmail.com>
2022-12-03 21:24:36 +0100

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,62 +1,21 @@
-// Add imports above this line
+// // Add imports above this line
 import SimpleLightbox from 'simplelightbox';
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 
 // Change code below this line
-console.log(galleryItems);
 
 const galleryContainer = document.querySelector('.gallery');
 const gallery = createGallery(galleryItems);
 
 galleryContainer.innerHTML = gallery;
-galleryContainer.addEventListener('click', clickOnGallery);
 
-function clickOnGallery(event) {
-  event.preventDefault();
-  if (event.target.nodeName !== 'IMG') {
-    return;
-  }
-  console.log(event.target.nodeName);
-  let instance = new SimpleLightbox('.gallery a', {
-    scrollZoom: false,
-    captionsData: 'alt',
-    captionDelay: 300,
-  });
-}
-
-// function clickOnGallery(event) {
-//   event.preventDefault();
-//   if (event.target.nodeName !== 'IMG') {
-//     return;
-//   }
-//   console.log(event.target.nodeName);
-
-//   const instance = basicLightbox.create(
-//     `
-//       <div class="modal">
-//          <img src="${event.target.dataset.source}" alt="Big Pictures" width="800" height="600">
-//       </div>
-//   `,
-//     {
-//       onShow: instance => {
-//         galleryContainer.addEventListener('keyup', closeButton);
-//       },
-//       onClose: instance => {
-// galleryContainer.removeEventListener('keyup', closeButton);
-//       },
-//     }
-//   );
-
-//   function closeButton(event) {
-//     if (event.key === 'Escape') {
-//       instance.close();
-//     }
-//   }
-
-//   instance.show();
-// }
+let instance = new SimpleLightbox('.gallery a', {
+  scrollZoom: false,
+  captionsData: 'alt',
+  captionDelay: 300,
+});
 
 function createGallery(galleryItems) {
   return galleryItems
